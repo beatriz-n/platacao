93cf72af6a415d5500bdc67b4b2a16c806cc5c05
beatriz <beatriznespolid@gmail.com>
2022-08-17 16:18:22 -0300

    layout

---
+++ prod/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-main class="body">
+      <v-sheet class="box-container"
+  height="530"
+  width="600"
+>
+<v-sheet
+  color="white"
+  height="450"
+  width="555"
+>
+<CamposComp></CamposComp>
+</v-sheet>
+</v-sheet>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import CamposComp from './components/camposComp.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    CamposComp
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
+<style scoped>
+.v-sheet{
+  margin-left: 3.4%;
+  margin-top: 3%;
+  padding-top: 1%;
+   border:solid rgba(216,216,216,255) 1.3px;
+    border-radius: 0.1rem;
+}
+.box-bottom{
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+}
+a{
+  /* color: rgb(106, 193, 244); */
+  color: rgb(253, 127, 127);
+}
+p{
+  text-align: center;
+  font-size: small;
+  padding-top: 1.3rem;
+}
+.box-container{
+  background-color:rgba(250,250,250,255);
+  margin-left: 35%;
+}
+.body{
+  background-color: rgba(245,245,245,255);
+}
+</style>
---
+++ prod/src/components/camposComp.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="register">
+    <div class="form">
+      <div class="input-form">
+        <input type="text" placeholder="Propriedade" />
+        </div>
+        <input type="text" placeholder="Plantação" />
+        <br>
+            <v-row>
+    <v-col cols="12">
+      <v-select
+        :items="items"
+        readonly
+        label="Read-only"
+      ></v-select>
+    </v-col>
+  </v-row>
+        <v-divider>ou</v-divider>
+      <v-row no-gutters>
+        <v-row
+          class="pa-3"
+        >
+           <v-btn  class="btn-register"
+>salvar</v-btn>
+        </v-row>
+        <br>
+        <v-row
+          class="pa-3"
+        >
+         <v-btn  class="btn-danger"
+>Cancelar</v-btn>
+        </v-row>
+    </v-row>
+       </div>
+        </div>
+</template>
+<script>
+export default {
+  name: 'camposComp',
+  data: () => ({
+    items: ['Foo', 'Bar', 'Fizz', 'Buzz']
+  })
+}
+</script>
+<style scoped>
+.space {
+  padding-left: 1rem;
+  display: flex;
+  align-content: center;
+}
+.register {
+  align-items: center;
+}
+h1 {
+  color: black;
+  margin-bottom: 1rem;
+}
+h5 {
+  color: rgb(163, 163, 163);
+  margin-right: 30px;
+  margin-left: 30px;
+}
+.form {
+  text-align: center;
+  margin-top: 5rem;
+}
+.v-btn {
+  display: flex;
+  /* background-color: rgba(1,149,247,255); */
+  background-color: rgb(205, 40, 40);
+  color: aliceblue;
+  height: 1.5rem;
+  margin-left: 10px;
+  font-size: 10px;
+  width: 14rem;
+  margin-top: 1rem;
+}
+.btn-register {
+  background-color: rgba(179, 223, 250, 255);
+  color: aliceblue;
+  transition: 0.5s;
+}
+.btn-register:hover {
+  background-color: rgba(1, 149, 247, 255);
+  color: aliceblue;
+}
+.v-divider {
+  margin: 30px;
+}
+.v-text-field {
+  display: flex;
+}
+input {
+  border: solid rgba(216, 216, 216, 255) 1.3px;
+  border-radius: 0.3rem;
+  margin: 2px;
+  background-color: rgba(250, 250, 250, 255);
+  width: 30rem;
+  height: 2rem;
+  color: rgb(33, 33, 33);
+  font-size: 12px;
+  padding-left: 0.7rem;
+}
+.input-form {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+img {
+  width: 3rem;
+}
+</style>
